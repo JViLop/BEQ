c1c0560160b0b2b3d14059527b034d13edfee626
JViLop <joanvilo97@gmail.com>
2025-05-02 10:08:26 -0500

    make command files all .bash

--- run_edks.bash
+++ run_edks.bash
@@ -1,5 +1,5 @@
 
-name=Tohoku
+name=Iquique
 python utils/run_ensemble_edks_GFs.py $name
 mpirun -n 4 python utils/run_ensemble_edks_okada_parallel.py $name
 python utils/run_edks_mean_errors_parallel.py $name
